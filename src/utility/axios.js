057e9a842203e418bf0c4affe769ccd5574bea61
Ethiogit9 <e553c5@gmail.com>
2025-04-08 10:11:58 -0400

    added backend link

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL = import.meta.env.BASE_URL;
 const instance = axios.create({
-  baseURL: "http://localhost:5002/",
+  baseURL: "https://forum-backend-j0y6.onrender.com/",
 });
 
 export default instance;
